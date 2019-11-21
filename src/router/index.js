c55336c7c1e122e50c9a23ce88a6c48759ab14a1
caesar <newman0934@gmail.com>
2019-11-21 16:25:53 +0800

    login with axois and toast

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
   {
     path:"/",
     name:"root",
-    redirect: "/restaurants"
+    redirect: "/signin"
   },
   {
     path:"/admin",
--- src/views/signIn.vue
+++ src/views/signIn.vue
@@ -8,28 +8,47 @@
       <!-- email輸入欄位 -->
       <div class="form-label-group mb-2">
         <label for="email">email</label>
-        <input id="email" name="email" v-model="email" type="email" class="form-control" placeholder="email"
-          required autofocus>
+        <input
+          id="email"
+          name="email"
+          v-model="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          required
+          autofocus
+        />
       </div>
 
       <!-- password輸入欄位 -->
       <div class="form-label-group mb-3">
         <label for="password">Password</label>
-        <input id="password" name="password" type="password" v-model="password" class="form-control" 
-          placeholder="Password" required>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          v-model="password"
+          class="form-control"
+          placeholder="Password"
+          required
+        />
       </div>
 
       <!-- 送出表單按鈕 -->
-      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">Submit</button>
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+        :disabled="isProcessing"
+      >Submit</button>
 
       <!-- 註冊頁面 -->
       <div class="text-center mb-3">
-        <p><router-link to="/signup">Sign Up</router-link></p>
+        <p>
+          <router-link to="/signup">Sign Up</router-link>
+        </p>
       </div>
 
-      <p class="mt-5 mb-3 text-muted text-center">
-        &copy; 2017-2018
-      </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
     </form>
   </div>
 </template>
@@ -37,25 +56,57 @@
 <script>
 /* eslint-disable */
 
+import authorizationAPI from "../apis/authorization.js";
+import { Toast } from "./../utils/helpers";
+
 export default {
   name: "SignIn",
 
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      isProcessing: false
     };
   },
-  
-  methods: {
-    handleSubmit(e) {
-      const data = JSON.stringify({
-        email: this.email,
-        password: this.password
-      });
 
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log("data", data);
+  methods: {
+    async handleSubmit(e) {
+      try {
+        if (!this.email || !this.password) {
+          Toast.fire({
+            type: "warning",
+            title: "請填入email和password"
+          });
+          return;
+        }
+
+        this.isProcessing = true;
+
+        const response = await authorizationAPI.signIn({
+          email: this.email,
+          password: this.password
+        });
+        console.log(response)
+        const { data, statusText } = response;
+
+        if (statusText !== "OK" || data.status != "success") {
+          throw new Error(statusText);
+        }
+        // 將 token 存放在 localStorage 內
+        localStorage.setItem("token", data.token);
+        // 成功登入後轉址到餐聽首頁
+        this.$router.push("/restaurants");
+
+      } catch (error) {
+        this.password = "";
+        this.isProcessing = false;
+
+        Toast.fire({
+          type: "warning",
+          title: "請確認你輸入的帳號密碼"
+        });
+      }
     }
   }
 };
