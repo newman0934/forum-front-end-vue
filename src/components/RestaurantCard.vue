ea8c0a4f2689c484f47c11d7eb11d7922680d52e
caesar <newman0934@gmail.com>
2019-11-22 16:07:17 +0800

    API restaurants addfavorite deletefavorite addlike deletelike

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,46 +1,72 @@
 <template>
   <div class="col-md-6 col-lg-4">
-
     <div class="card mb-4">
+      <img
+        class="card-img-top"
+        :src="restaurant.image"
+        alt="Card image cap"
+        width="286px"
+        height="180px"
+      />
 
-      <img class="card-img-top" :src="restaurant.image" alt="Card image cap" width="286px" height="180px">
-      
       <div class="card-body">
-
-        <p class="card-text title-wrap"><router-link :to="{name: 'restaurant', params:{id: restaurant.id}}">{{restaurant.name}}</router-link></p>
-        <span class="badge badge-secondary">{{restaurant.Category.name}}</span>
-        <p class="card-text text-truncate">{{restaurant.description }}</p>
-
+        <p class="card-text title-wrap">
+          <router-link
+            :to="{ name: 'restaurant', params: { id: restaurant.id } }"
+            >{{ restaurant.name }}</router-link
+          >
+        </p>
+        <span class="badge badge-secondary">{{
+          restaurant.Category.name
+        }}</span>
+        <p class="card-text text-truncate">{{ restaurant.description }}</p>
       </div>
 
       <div class="card-footer">
-
-        <button v-if="restaurant.isFavorited" type="button" class="btn btn-danger btn-border favorite mr-2"
-          @click.stop.prevent="deleteFavorite">
+        <button
+          v-if="restaurant.isFavorited"
+          type="button"
+          class="btn btn-danger btn-border favorite mr-2"
+          @click.stop.prevent="deleteFavorite(restaurant.id)"
+        >
           移除最愛
         </button>
 
-        <button v-else type="button" class="btn btn-primary btn-border favorite mr-2" @click.stop.prevent="addFavorite">
+        <button
+          v-else
+          type="button"
+          class="btn btn-primary btn-border favorite mr-2"
+          @click.stop.prevent="addFavorite(restaurant.id)"
+        >
           加到最愛
         </button>
 
-        <button v-if="restaurant.isLiked" type="button" class="btn btn-danger like mr-2" @click.stop.prevent="deleteLike">
+        <button
+          v-if="restaurant.isLiked"
+          type="button"
+          class="btn btn-danger like mr-2"
+          @click.stop.prevent="deleteLike(restaurant.id)"
+        >
           Unlike
         </button>
 
-        <button v-else type="button" class="btn btn-primary like mr-2" @click.stop.prevent="addLike">
+        <button
+          v-else
+          type="button"
+          class="btn btn-primary like mr-2"
+          @click.stop.prevent="addLike(restaurant.id)"
+        >
           Like
         </button>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 export default {
-  name: "RestaurantCard",
-
   props: {
     initialRestaurant: {
       type: Object,
@@ -55,32 +81,93 @@ export default {
   },
 
   methods: {
-    addFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: true
-      };
+    // STEP 2: 將 addFavorite 改成 async...await 語法
+    async addFavorite(restaurantId) {
+      try {
+        // STEP 3: 使用撰寫好的 addFavorite 方法去呼叫 API，並取得回傳內容
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+
+        // STEP 4: 若請求過程有錯，則進到錯誤處理
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        // STEP 5: 請求成功的話，改變 Vue 內的資料狀態
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: true
+        };
+      } catch (error) {
+        // STEP 6: 請求失敗的話則跳出錯誤提示
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳加入最愛，請稍後再試"
+        });
+      }
     },
 
-    deleteFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: false
-      };
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳從最愛移除，請稍後再試"
+        });
+      }
+    },
+    async addLike(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addLike({ restaurantId });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: true
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法按讚，請稍後再試"
+        });
+      }
     },
 
-    addLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: true
-      };
-    },
+    async deleteLike(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteLike({
+          restaurantId
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
 
-    deleteLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: false
-      };
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳刪除，請稍後再試"
+        });
+      }
     }
   }
 };
