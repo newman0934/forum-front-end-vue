a4430b20044f025d5e8381c24634f429db88290a
caesar <newman0934@gmail.com>
2019-11-22 19:01:02 +0800

    API adminRestaurantNew

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.stop.prevent="handleSubmit">
+  <form v-show="!isLoading" @submit.stop.prevent="handleSubmit">
     <div class="form-group">
       <label for="name">Name</label>
       <input
@@ -97,40 +97,19 @@
       />
     </div>
 
-    <button type="submit" class="btn btn-primary">送出</button>
+    <button
+      type="submit"
+      class="btn btn-primary"
+      :disabled="isProcessing"
+    >{{isProcessing ? "處理中..." : "送出"}}</button>
   </form>
 </template>
 
 <script>
 /* eslint-disable */
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z"
-    },
-    {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z"
-    },
-    {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z"
-    },
-    {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z"
-    }
-  ]
-};
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
+
 export default {
   data() {
     return {
@@ -143,7 +122,8 @@ export default {
         image: "",
         openingHours: ""
       },
-      categories: []
+      categories: [],
+      isLoading: true
     };
   },
   props: {
@@ -158,6 +138,10 @@ export default {
         image: "",
         openingHours: ""
       })
+    },
+    isProcessing: {
+      type: Boolean,
+      default: false
     }
   },
   created() {
@@ -168,8 +152,21 @@ export default {
     };
   },
   methods: {
-    fetchCategories() {
-      this.categories = dummyData.categories;
+    async fetchCategories() {
+      try {
+        const { data, statusText } = await adminAPI.categories.get();
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.categories = data.categories;
+        this.isLoading = false;
+      } catch (error) {
+        this.isLoading = false;
+        Toast.fire({
+          type: "error",
+          title: "無法取得餐廳類別，請稍後再試"
+        });
+      }
     },
     handleFileChange(e) {
       const files = e.target.files;
@@ -181,6 +178,19 @@ export default {
       this.restaurant.image = imageURL;
     },
     handleSubmit(e) {
+      if (!this.restaurant.name) {
+        Toast.fire({
+          type: "warning",
+          title: "請填寫餐廳名稱"
+        });
+        return;
+      } else if (!this.restaurant.categoryId) {
+        Toast.fire({
+          type: "warning",
+          title: "請選擇餐廳類別"
+        });
+        return;
+      }
       const form = e.target;
       const formData = new FormData(form);
       this.$emit("after-submit", formData);
--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm :is-processing="isProcessing" @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
 <script>
 /* eslint-disable */
 import AdminRestaurantForm from "../components/AdminRestaurantForm";
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
     AdminRestaurantForm
   },
+  data(){
+    return {
+      isProcessing:false
+    }
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ": " + value);
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const { data, statusText } = await adminAPI.restaurants.create({
+          formData
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.$router.push({ name: "admin-restaurants" });
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          type: "error",
+          title: "無法建立餐廳，請稍後再試"
+        });
       }
     }
   }
